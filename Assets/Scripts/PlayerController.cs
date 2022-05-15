5eac3818f7866e16ff6f6462a154cd3d234ad492
GeekmasterK <ktheiss1990@gmail.com>
2022-05-15 13:27:07 -0700

    Added a boost mechanic. The surface effector speeds up when the player is pressing the "up" input.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,24 +6,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
-    Rigidbody2D rb2D;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
 
+    Rigidbody2D rb2D;
+    SurfaceEffector2D se2D;
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        se2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxisRaw("Horizontal") < 0f)
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetAxisRaw("Horizontal") < 0f)
         {
             rb2D.AddTorque(torqueAmount);
         }
-        else if(Input.GetAxisRaw("Horizontal") > 0f)
+        else if (Input.GetAxisRaw("Horizontal") > 0f)
         {
             rb2D.AddTorque(-torqueAmount);
+        }
+    }
+
+    void RespondToBoost()
+    {
+        if(Input.GetAxisRaw("Vertical") > 0f)
+        {
+            se2D.speed = boostSpeed;
+        }
+        else
+        {
+            se2D.speed = baseSpeed;
         }
     }
 }
